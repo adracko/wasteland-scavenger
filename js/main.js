a34adefce98423e99413f50592c7dba63d693e5e
adracko <adracko@gmail.com>
2015-12-30 09:23:05 -0700

    comparing data

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   $('#nav-top').children().children().click(function(){
     $('#nav-top').children().addClass('inactive').removeClass('active')
     $(this).parent().addClass('active').removeClass('inactive')
-
+    var className = $(this).data('section')
+    var data = $('#screen').children().data('section') === className
+    /**$('#screen').children().addClass('hidden')**/
+    $(data).append('hidden')
   });
 });
